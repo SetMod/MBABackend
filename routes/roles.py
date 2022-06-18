3f7610cbe74e1dd8197d405786df23aae6bafb32
SetMod <voroncka.andrey@gmail.com>
2022-06-18 12:54:21 +0300

    feat: added beter response for roles

--- routes/roles.py
+++ routes/roles.py
@@ -10,18 +10,18 @@ roles_service = RolesService()
 def get_all_roles():
     roles = roles_service.get_all_roles()
 
-    if roles is None:
-        return 'Roles not found', 404
-    else:
+    if isinstance(roles, list):
         return jsonify(roles), 200
+    else:
+        return roles, 404
 
 
 @roles_api.get('/<int:role_id>')
 def get_role_by_id(role_id: int):
     role = roles_service.get_role_by_id(role_id)
 
-    if role is None:
-        return 'Role not found', 404
+    if isinstance(role, str):
+        return role, 404
     else:
         return jsonify(role), 200
 
@@ -34,8 +34,8 @@ def create_role():
 
     new_role = roles_service.create_role(role)
 
-    if new_role is None:
-        return "Failed to create a role", 400
+    if isinstance(new_role, str):
+        return new_role, 400
     else:
         return jsonify(new_role), 201
 
@@ -48,8 +48,8 @@ def update_role(role_id: int):
 
     updated_role = roles_service.update_role(role_id, role)
 
-    if updated_role is None:
-        return 'Failed to update a role', 400
+    if isinstance(role, str):
+        return updated_role, 400
     else:
         return jsonify(updated_role), 200
 
@@ -58,7 +58,7 @@ def update_role(role_id: int):
 def delete_role(role_id: int):
     deleted_role = roles_service.delete_role(role_id)
 
-    if deleted_role is None:
-        return 'Failed to delete a role', 400
+    if isinstance(deleted_role, str):
+        return deleted_role, 400
     else:
         return jsonify(deleted_role), 200
--- services/RolesService.py
+++ services/RolesService.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from marshmallow import ValidationError
 from models.RolesModel import Roles, RolesSchema
 from app import db
@@ -9,59 +10,88 @@ class RolesService:
         self.roles_schema = RolesSchema()
 
     def get_all_roles(self, dump: bool = True):
-        roles = db.session.query(Roles).all()
+        try:
+            roles = db.session.query(Roles).all()
 
-        if len(roles) > 0:
-            return self.roles_schema.dump(roles, many=True) if dump else roles
-        else:
-            return None
+            if len(roles) > 0:
+                return self.roles_schema.dump(roles, many=True) if dump else roles
+            else:
+                return 'Roles not found'
+        except Exception as err:
+            print(err)
+            return 'Failed to get roles'
 
     def get_role_by_id(self, id: int, dump: bool = True):
-        role = db.session.query(Roles).where(Roles.role_id == id).first()
-
-        if isinstance(role, Roles):
-            return self.roles_schema.dump(role) if dump else role
-        else:
-            return None
-
-    def get_role_by_name(self, name: str, dump: bool = True) -> Roles:
-        role = db.session.query(Roles).filter_by(role_name=name).first()
-        return self.roles_schema.dump(role) if dump else role
-
-    def create_role(self, role: Roles, dump: bool = True):
         try:
-            db.session.add(role)
+            role = db.session.query(Roles).where(Roles.role_id == id).first()
+
+            if isinstance(role, Roles):
+                return self.roles_schema.dump(role) if dump else role
+            else:
+                return 'Role not found'
+        except Exception as err:
+            print(err)
+            return 'Failed to get role'
+
+    def get_role_by_name(self, name: str, dump: bool = True):
+        try:
+            role = db.session.query(Roles).filter_by(role_name=name).first()
+
+            if isinstance(role, Roles):
+                return self.roles_schema.dump(role) if dump else role
+            else:
+                return 'Role not found'
+        except Exception as err:
+            print(err)
+            return 'Failed to get role'
+
+    def create_role(self, new_role: Roles, dump: bool = True):
+        role = self.get_role_by_name(new_role.role_name, dump=False)
+        if isinstance(role, Roles):
+            return 'Role already exists'
+
+        try:
+            db.session.add(new_role)
             db.session.commit()
-            return self.roles_schema.dump(role) if dump else role
-        except Exception:
-            return None
+            return self.roles_schema.dump(new_role) if dump else new_role
+        except Exception as err:
+            print(err)
+            return 'Failed to create a role'
 
     def update_role(self, role_id: int, updated_role: Roles, dump: bool = True):
         role = self.get_role_by_id(role_id, dump=False)
 
+        if not isinstance(role, Roles):
+            return role
+
+        existing_role = self.get_role_by_name(
+            updated_role.role_name, dump=False)
+
+        if isinstance(existing_role, Roles):
+            return f'Role with name "{updated_role.role_name}" already exists'
+
+        role.role_name = updated_role.role_name
+        role.role_description = updated_role.role_description
         try:
-            if isinstance(role, Roles):
-                role.role_name = updated_role.role_name
-                role.role_description = updated_role.role_description
-                db.session.commit()
-                return self.roles_schema.dump(role) if dump else role
-            else:
-                return None
-        except Exception:
-            return None
+            db.session.commit()
+            return self.roles_schema.dump(role) if dump else role
+        except Exception as err:
+            print(err)
+            return 'Failed to update a role'
 
     def delete_role(self, role_id: int, dump: bool = True):
         role = self.get_role_by_id(role_id, dump=False)
 
+        if not isinstance(role, Roles):
+            return role
+
         try:
-            if isinstance(role, Roles):
-                db.session.delete(role)
-                db.session.commit()
-                return self.roles_schema.dump(role) if dump else role
-            else:
-                return None
-        except Exception:
-            return None
+            db.session.delete(role)
+            db.session.commit()
+            return self.roles_schema.dump(role) if dump else role
+        except Exception as err:
+            print(err)
+            return 'Failed to delete a role'
 
     def map_role(self, role_dict: dict):
         try:
