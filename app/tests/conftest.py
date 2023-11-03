7e5bdb292896cd27176ab393961b262445b8955e
SetMod <voroncka.andrey@gmail.com>
2023-11-03 20:02:47 +0200

    Updated tests

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -51,13 +51,13 @@ def roles(request, db):
     class Roles(db.Model):
         __tablename__ = "roles"
         role_id = db.Column('role_id', db.Integer, primary_key=True)
-        role_name = db.Column('role_name', db.String(50),
+        name = db.Column('name', db.String(50),
                               unique=True, nullable=False)
-        role_description = db.Column(
-            'role_description', db.Text, nullable=False)
+        description = db.Column(
+            'description', db.Text, nullable=False)
 
         def __repr__(self):
-            return f'<Role(role_id={self.role_id},role_name={self.role_name},role_description={self.role_description})>'
+            return f'<Role(role_id={self.role_id},name={self.name},description={self.description})>'
     # Create tables
     db.create_all()
 
@@ -83,19 +83,19 @@ def roles_schema(ma, roles):
 #         __tablename__ = "users"
 
 #         user_id = db.Column('user_id', db.Integer, primary_key=True)
-#         user_first_name = db.Column(
-#             'user_first_name', db.String(100), nullable=False)
-#         user_second_name = db.Column(
-#             'user_second_name', db.String(100), nullable=False)
-#         user_email = db.Column('user_email', db.String(
+#         first_name = db.Column(
+#             'first_name', db.String(100), nullable=False)
+#         second_name = db.Column(
+#             'second_name', db.String(100), nullable=False)
+#         email = db.Column('email', db.String(
 #             255), unique=True, nullable=False)
-#         user_phone = db.Column('user_phone', db.String(18), unique=True)
-#         user_username = db.Column('user_username', db.String(
+#         phone = db.Column('phone', db.String(18), unique=True)
+#         username = db.Column('username', db.String(
 #             50), unique=True, nullable=False)
-#         user_password = db.Column(
-#             'user_password', db.String(50), nullable=False)
-#         user_create_date = db.Column(
-#             'user_create_date', db.DateTime, default=datetime.utcnow)
+#         password = db.Column(
+#             'password', db.String(50), nullable=False)
+#         create_date = db.Column(
+#             'create_date', db.DateTime, default=datetime.utcnow)
 #         role_id = db.Column('role_id', db.Integer, db.ForeignKey(
 #             "roles.role_id"), nullable=False)
 
@@ -104,7 +104,7 @@ def roles_schema(ma, roles):
 #             "UsersOrganizations", backref="user", cascade='save-update, merge, delete')
 
 #         def __repr__(self):
-#             return f'<User(user_id={self.user_id},user_first_name={self.user_first_name},user_second_name={self.user_second_name},user_email={self.user_email},user_username={self.user_username},user_create_date={self.user_create_date},role_id={self.role_id})>'
+#             return f'<User(user_id={self.user_id},first_name={self.first_name},second_name={self.second_name},email={self.email},username={self.username},create_date={self.create_date},role_id={self.role_id})>'
 
 #     # Create tables
 #     db.create_all()
--- app/tests/unit/test_models.py
+++ app/tests/unit/test_models.py
@@ -1,13 +1,13 @@
 
 def test_new_role(db, roles):
-    user_role = roles(role_name='User',
-                      role_description='Roles for standard user')
+    user_role = roles(name='User',
+                      description='Roles for standard user')
     db.session.add(user_role)
     db.session.commit()
 
     assert user_role.role_id == 1
-    assert user_role.role_name == 'User'
-    assert user_role.role_description == 'Roles for standard user'
+    assert user_role.name == 'User'
+    assert user_role.description == 'Roles for standard user'
 
 
 def test_get_all_roles(db, roles):
@@ -17,22 +17,22 @@ def test_get_all_roles(db, roles):
 
 
 def test_get_role_by_name(db, roles):
-    role = db.session.query(roles).where(roles.role_name == 'User').first()
+    role = db.session.query(roles).where(roles.name == 'User').first()
 
     assert isinstance(role, roles)
-    assert role.role_id == 1
-    assert role.role_name == 'User'
-    assert role.role_description == 'Roles for standard user'
+    assert role.id == 1
+    assert role.name == 'User'
+    assert role.description == 'Roles for standard user'
 
 
 def test_role_schema_dum(db, roles, roles_schema):
-    role = db.session.query(roles).where(roles.role_name == 'User').first()
+    role = db.session.query(roles).where(roles.name == 'User').first()
     dump_role = roles_schema.dump(role)
 
     assert isinstance(dump_role, dict)
-    assert dump_role['role_id'] == 1
-    assert dump_role['role_name'] == 'User'
-    assert dump_role['role_description'] == 'Roles for standard user'
+    assert dump_role['id'] == 1
+    assert dump_role['name'] == 'User'
+    assert dump_role['description'] == 'Roles for standard user'
 
 
 def test_role_schema_dump_many(db, roles, roles_schema):
@@ -43,17 +43,17 @@ def test_role_schema_dump_many(db, roles, roles_schema):
     assert isinstance(dump_role[0], dict)
 
 # def test_new_user():
-#     user = Users(user_first_name='Testfirstname',
-#                  user_second_name='Testsecondname',
-#                  user_email='test@test.com',
-#                  user_phone='+01234567890',
-#                  user_username='testuser',
-#                  user_password='testpass',
+#     user = Users(first_name='Testfirstname',
+#                  second_name='Testsecondname',
+#                  email='test@test.com',
+#                  phone='+01234567890',
+#                  username='testuser',
+#                  password='testpass',
 #                  role_id=1)
-#     assert user.user_first_name == 'Testfirstname'
-#     assert user.user_second_name == 'Testsecondname'
-#     assert user.user_email == 'test@test.com'
-#     assert user.user_phone == '+01234567890'
-#     assert user.user_username == 'testuser'
-#     assert user.user_password == 'testpass'
+#     assert user.first_name == 'Testfirstname'
+#     assert user.second_name == 'Testsecondname'
+#     assert user.email == 'test@test.com'
+#     assert user.phone == '+01234567890'
+#     assert user.username == 'testuser'
+#     assert user.password == 'testpass'
 #     assert user.role_id == 1
