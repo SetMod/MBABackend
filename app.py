25036507180b7e5a4574557259518b4393c96f2f
SetMod <voroncka.andrey@gmail.com>
2023-11-04 22:30:54 +0200

    Moved blueprints registrations to routes init

--- app.py
+++ app.py
@@ -1,29 +1,5 @@
 from app import app
-from app.routes import (
-    roles_api,
-    organization_roles_api,
-    organizations_api,
-    users_api,
-    users_organizations_api,
-    files_api,
-    reports_api,
-    analyzes_api,
-    visualizations_api,
-)
-
-
-app.register_blueprint(roles_api, url_prefix="/api/roles")
-app.register_blueprint(organization_roles_api, url_prefix="/api/organization_roles")
-app.register_blueprint(organizations_api, url_prefix="/api/organizations")
-app.register_blueprint(users_api, url_prefix="/api/users")
-app.register_blueprint(users_organizations_api, url_prefix="/api/users_organizations")
-app.register_blueprint(files_api, url_prefix="/api/files")
-app.register_blueprint(reports_api, url_prefix="/api/reports")
-app.register_blueprint(analyzes_api, url_prefix="/api/analyzes")
-app.register_blueprint(visualizations_api, url_prefix="/api/visualizations")
+from app.config import APP_DEBUG, APP_LISTEN_HOST, APP_LISTEN_PORT
 
 if __name__ == "__main__":
-    host = "localhost"
-    port = 8000
-    debug = True
-    app.run(host, port, debug)
+    app.run(APP_LISTEN_HOST, APP_LISTEN_PORT, APP_DEBUG)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,15 +1,20 @@
-from app.routes.roles import roles_api, roles_service
-from app.routes.organization_roles import (
-    organization_roles_api,
-    organization_roles_service,
-)
-from app.routes.users_organizations import (
-    users_organizations_api,
-    users_organizations_service,
-)
-from app.routes.users import users_api, users_service
-from app.routes.organizations import organizations_api, organizations_service
-from app.routes.analyzes import analyzes_api, analyzes_service
-from app.routes.files import files_api, files_service
-from app.routes.reports import reports_api, reports_service
-from app.routes.visualizations import visualizations_api, visualizations_service
+from app.routes.roles import roles_api
+from app.routes.organization_roles import organization_roles_api
+from app.routes.users_organizations import users_organizations_api
+from app.routes.users import users_api
+from app.routes.organizations import organizations_api
+from app.routes.analyzes import analyzes_api
+from app.routes.files import files_api
+from app.routes.reports import reports_api
+from app.routes.visualizations import visualizations_api
+from app import app
+
+app.register_blueprint(roles_api, url_prefix="/api/v1/roles")
+app.register_blueprint(organization_roles_api, url_prefix="/api/v1/organization_roles")
+app.register_blueprint(organizations_api, url_prefix="/api/v1/organizations")
+app.register_blueprint(users_api, url_prefix="/api/v1/users")
+app.register_blueprint(users_organizations_api, url_prefix="/api/v1/users_organizations")
+app.register_blueprint(files_api, url_prefix="/api/v1/files")
+app.register_blueprint(reports_api, url_prefix="/api/v1/reports")
+app.register_blueprint(analyzes_api, url_prefix="/api/v1/analyzes")
+app.register_blueprint(visualizations_api, url_prefix="/api/v1/visualizations")
