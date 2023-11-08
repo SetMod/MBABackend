4a5b8fcd4c0d43abfdc66de83e6c2d585a72678f
SetMod <voroncka.andrey@gmail.com>
2023-11-09 00:56:20 +0200

    Updated GenericService

--- app/services/GenericService.py
+++ app/services/GenericService.py
@@ -1,9 +1,11 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow import ValidationError
 from flask_sqlalchemy.model import Model
-import sqlalchemy as sa
+from datetime import datetime
+from app.config import logger
 from typing import Union
-from app import db
+import sqlalchemy as sa
+from app.init import db
 
 
 class GenericService:
@@ -11,18 +13,58 @@ class GenericService:
         self.schema = schema
         self.model_class = model_class
 
-    def get_all(self, dump: bool = True) -> Union[list[Model], list[dict]]:
+    def get_all(
+        self,
+        page: Union[int, None] = None,
+        per_page: Union[int, None] = None,
+        max_per_page: Union[int, None] = None,
+        count: bool = True,
+        dump: bool = True,
+    ) -> Union[list[Model], list[dict]]:
         result = db.session.query(self.model_class).all()
+        # result = db.paginate(
+        #     select=db.session.query(self.model_class).all(),
+        #     page=page,
+        #     per_page=per_page,
+        #     max_per_page=max_per_page,
+        #     count=count,
+        # )
         return self.schema.dump(result, many=True) if dump else result
 
-    def get_by_id(self, id: int, dump: bool = True) -> Union[Model, dict]:
+    def get_by_id(self, id: int, dump: bool = True) -> Union[Model, dict, None]:
         result = (
             db.session.query(self.model_class).where(self.model_class.id == id).first()
         )
+
+        if not result:
+            return None
+
         return self.schema.dump(result) if dump else result
 
-    def get_by_name(self, name: str, dump: bool = True) -> Union[Model, dict]:
+    def get_by_field(
+        self, field_name: str, field_value, dump: bool = True
+    ) -> Union[Model, dict, None]:
+        if not hasattr(self.model_class, field_name):
+            return None
+        value = getattr(self.model_class, field_name)
+
+        # Check if it works
+        if not isinstance(value, sa.Column):
+            return None
+
+        result = db.session.query(self.model_class).where(value == field_value).first()
+
+        if not result:
+            return None
+
+        return self.schema.dump(result) if dump else result
+
+    def get_by_name(self, name: str, dump: bool = True) -> Union[Model, dict, None]:
         result = db.session.query(self.model_class).filter_by(name=name).first()
+
+        if not result:
+            return None
+
         return self.schema.dump(result) if dump else result
 
     def create(self, model: Model, dump: bool = True) -> Union[Model, dict]:
@@ -32,7 +74,7 @@ class GenericService:
 
     def update(
         self, id: int, updated_model: Model, dump: bool = True
-    ) -> Union[Model, dict]:
+    ) -> Union[Model, dict, None]:
         model = self.get_by_id(id, dump=False)
 
         if not isinstance(model, self.model_class):
@@ -48,6 +90,7 @@ class GenericService:
                 continue
 
             attr_value = getattr(updated_model, attr_name)
+
             if not isinstance(attr_value, sa.Column):
                 continue
 
@@ -57,19 +100,31 @@ class GenericService:
         return self.schema.dump(model) if dump else model
 
     def delete(self, id: int, dump: bool = True):
-        result = self.get_by_id(id, dump=False)
+        model = self.get_by_id(id, dump=False)
 
-        if not isinstance(result, self.model_class):
-            return result
+        if not isinstance(model, self.model_class):
+            return model
 
-        db.session.delete(result)
+        db.session.delete(model)
         db.session.commit()
-        return self.schema.dump(result) if dump else result
+        return self.schema.dump(model) if dump else model
 
-    def map_model(self, data: dict):
+    def soft_delete(self, id: int, dump: bool = True):
+        model = self.get_by_id(id, dump=False)
+
+        if not isinstance(model, self.model_class):
+            return model
+
+        model.soft_deleted = True
+        model.deleted_date = datetime.utcnow()
+
+        db.session.commit()
+        return self.schema.dump(model) if dump else model
+
+    def map_model(self, model_dict: dict):
         try:
-            model = self.schema.load(data)
-            model = self.model_class(**data)
+            model = self.schema.load(model_dict)
+            model = self.model_class(**model_dict)
             return model
         except ValidationError as err:
             return err.messages
