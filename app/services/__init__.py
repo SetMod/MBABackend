620acbb42a07cbb46901c4d8840048b3b627633f
SetMod <voroncka.andrey@gmail.com>
2023-11-09 00:57:53 +0200

    Updated services init file

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,12 +1,10 @@
 from app.services.GenericService import GenericService
-# from app.services.RolesService import RolesService
-# from app.services.OrganizationRolesService import OrganizationRolesService
-# from app.services.UsersOrganizationsService import UsersOrganizationsService
-# from app.services.VisualizationsService import VisualizationsService
-# from app.services.FilesService import FilesService
-# # from app.services.UsersService import UsersService
-# # from app.services.OrganizationsService import OrganizationsService
-# from app.services.AnalyzesService import AnalyzesService
-# from app.services.ReportsService import ReportsService
-
-from app.services.services import *
+from app.services.RolesService import RolesService
+from app.services.OrganizationRolesService import OrganizationRolesService
+from app.services.UsersOrganizationsService import UsersOrganizationsService
+from app.services.UsersService import UsersService
+from app.services.OrganizationsService import OrganizationsService
+from app.services.VisualizationsService import VisualizationsService
+from app.services.FilesService import FilesService
+from app.services.ReportsService import ReportsService
+from app.services.AnalyzesService import AnalyzesService
