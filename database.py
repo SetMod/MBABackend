9a6793402c6fb4b30aeb440db51a54ee182405be
SetMod <voroncka.andrey@gmail.com>
2023-12-02 19:29:49 +0200

    Updated database.py

--- database.py
+++ database.py
@@ -1,30 +1,48 @@
+from flask import Flask
 from app.config import APP_DATABASE_PATH
 from app.db import db
 from app import create_app
+from app.models import Users
 import argparse
 import os
 
 
-def recreate_database():
-    db.drop_all()
-    db.create_all()
+def recreate_database(app: Flask):
+    drop_database(app)
+    create_database(app)
 
 
-def drop_database():
+def drop_database(app: Flask):
     print("Dropping database!")
-    db.drop_all()
+
+    with app.app_context():
+        db.drop_all()
+
     if os.path.exists(APP_DATABASE_PATH):
         print(f"Removing file: {APP_DATABASE_PATH}")
         os.remove(APP_DATABASE_PATH)
 
 
-def create_database():
+def create_database(app: Flask):
     if os.path.exists(APP_DATABASE_PATH):
         print(f"Database already exists at {os.path.abspath(APP_DATABASE_PATH)}")
         return
 
     print("Creating database!")
-    db.create_all()
+    with app.app_context():
+        db.create_all()
+
+        user = Users()
+        user.username = "guest"
+        user.password = "guest"
+        user.first_name = "First"
+        user.second_name = "Guest"
+        user.email = "guest@example.com"
+        user.phone = "+123456567123"
+        user.active = True
+
+        db.session.add(user)
+        db.session.commit()
 
 
 if __name__ == "__main__":
@@ -36,13 +54,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.create_db:
-        with app.app_context():
-            create_database()
+        create_database(app)
     elif args.delete_db:
-        with app.app_context():
-            drop_database()
+        drop_database(app)
     elif args.recreate_db:
-        with app.app_context():
-            recreate_database()
+        recreate_database(app)
     else:
         parser.print_help()
