064be2ea54bf0a6467fd81575ad425ed92d81b10
SetMod <voroncka.andrey@gmail.com>
2023-12-15 17:28:26 +0200

    Updated database.py

--- database.py
+++ database.py
@@ -1,8 +1,21 @@
 from flask import Flask
-from app.config import APP_DATABASE_PATH
+from app.config import APP_ANALYZES_FOLDER, APP_DATABASE_PATH, APP_UPLOAD_FOLDER
 from app.db import db
 from app import create_app
-from app.models import OrganizationMembers, OrganizationRoles, Organizations, Users, Roles
+from app.models import (
+    AnalyzeStatus,
+    Analyzes,
+    DatasourceTypes,
+    FileDatasources,
+    GenericReports,
+    OrganizationMembers,
+    OrganizationRoles,
+    Organizations,
+    ReportTypes,
+    Users,
+    Roles,
+    Algorithm,
+)
 import argparse
 import os
 
@@ -54,9 +67,41 @@ def create_database(app: Flask):
         member1.active = True
         member1.role = OrganizationRoles.OWNER
 
+        datasource1 = FileDatasources()
+        datasource1.name = "Transactions Datasource"
+        datasource1.type = DatasourceTypes.FILE
+        datasource1.file_path = (
+            APP_UPLOAD_FOLDER.joinpath("test_datasource_1.csv").resolve().as_posix()
+        )
+        datasource1.creator_id = 1
+
+        analyze1 = Analyzes()
+        analyze1.name = "Test analyze"
+        analyze1.description = "Test analyze for demo"
+        analyze1.algorithm = Algorithm.APRIORI
+        analyze1.confidence = 0.1
+        analyze1.support = 1
+        analyze1.lift = 1.5
+        analyze1.rules_length = 2
+        analyze1.status = AnalyzeStatus.NOT_STARTED
+        analyze1.file_path = (
+            APP_ANALYZES_FOLDER.joinpath("test_analyze_1.csv").resolve().as_posix()
+        )
+        analyze1.datasource_id = 1
+        analyze1.creator_id = 1
+
+        db.session.add(analyze1)
+
+        report1 = GenericReports()
+        report1.name = "Transactions Report"
+        report1.type = ReportTypes.GENERIC
+        report1.creator_id = 1
+
         db.session.add(user)
         db.session.add(org1)
         db.session.add(member1)
+        db.session.add(datasource1)
+        db.session.add(report1)
         db.session.commit()
 
 
